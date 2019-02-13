10bf01e177121cf210747b49453a10eb187c9704
Christopher Pietsch <cpietsch@gmail.com>
2019-02-13 20:59:20 +1300

    removed encodeURIComponent from detail url

--- js/canvas.js
+++ js/canvas.js
@@ -798,7 +798,7 @@ function Canvas() {
         }
 
         // console.log("load", d)
-        var url = encodeURIComponent(config.loader.textures.detail.url + d.id + '.jpg')
+        var url = config.loader.textures.detail.url + d.id + '.jpg'
         var texture = new PIXI.Texture.fromImage(url, true)
         var sprite = new PIXI.Sprite(texture);
 
