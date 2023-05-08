410e832da90d0b1befae4943221691c764b46a1b
Christopher Pietsch <cpietsch@gmail.com>
2023-05-08 17:32:15 +0200

    fixed layout selection switch

--- js/viz.js
+++ js/viz.js
@@ -1,36 +1,34 @@
-//                            ,--.                                     
-//                ,---,   ,--/  /|               .--.--.               
-//        ,---.,`--.' |,---,': / '         ,--, /  /    '.             
-//       /__./||   :  ::   : '/ /        ,'_ /||  :  /`. /             
-//  ,---.;  ; |:   |  '|   '   ,    .--. |  | :;  |  |--`              
-// /___/ \  | ||   :  |'   |  /   ,'_ /| :  . ||  :  ;_                
-// \   ;  \ ' |'   '  ;|   ;  ;   |  ' | |  . . \  \    `.             
-//  \   \  \: ||   |  |:   '   \  |  | ' |  | |  `----.   \            
-//   ;   \  ' .'   :  ;|   |    ' :  | | :  ' ;  __ \  \  |            
-//    \   \   '|   |  ''   : |.  \|  ; ' |  | ' /  /`--'  /            
-//     \   `  ;'   :  ||   | '_\.':  | : ;  ; |'--'.     /             
-//      :   \ |;   |.' '   : |    '  :  `--'   \ `--'---'              
-//       '---" '---'   ;   |,'    :  ,      .-./                       
-//                     '---'       `--`----'                           
-//                ,---,    ,---,.           .---.    ,---,.,-.----.    
-//        ,---.,`--.' |  ,'  .' |          /. ./|  ,'  .' |\    /  \   
-//       /__./||   :  :,---.'   |      .--'.  ' ;,---.'   |;   :    \  
-//  ,---.;  ; |:   |  '|   |   .'     /__./ \ : ||   |   .'|   | .\ :  
-// /___/ \  | ||   :  |:   :  |-, .--'.  '   \' .:   :  |-,.   : |: |  
-// \   ;  \ ' |'   '  ;:   |  ;/|/___/ \ |    ' ':   |  ;/||   |  \ :  
-//  \   \  \: ||   |  ||   :   .';   \  \;      :|   :   .'|   : .  /  
-//   ;   \  ' .'   :  ;|   |  |-, \   ;  `      ||   |  |-,;   | |  \  
-//    \   \   '|   |  ''   :  ;/|  .   \    .\  ;'   :  ;/||   | ;\  \ 
-//     \   `  ;'   :  ||   |    \   \   \   ' \ ||   |    \:   ' | \.' 
-//      :   \ |;   |.' |   :   .'    :   '  |--" |   :   .':   : :-'   
-//       '---" '---'   |   | ,'       \   \ ;    |   | ,'  |   |.'     
-//                     `----'          '---"     `----'    `---'       
-
+//                            ,--.
+//                ,---,   ,--/  /|               .--.--.
+//        ,---.,`--.' |,---,': / '         ,--, /  /    '.
+//       /__./||   :  ::   : '/ /        ,'_ /||  :  /`. /
+//  ,---.;  ; |:   |  '|   '   ,    .--. |  | :;  |  |--`
+// /___/ \  | ||   :  |'   |  /   ,'_ /| :  . ||  :  ;_
+// \   ;  \ ' |'   '  ;|   ;  ;   |  ' | |  . . \  \    `.
+//  \   \  \: ||   |  |:   '   \  |  | ' |  | |  `----.   \
+//   ;   \  ' .'   :  ;|   |    ' :  | | :  ' ;  __ \  \  |
+//    \   \   '|   |  ''   : |.  \|  ; ' |  | ' /  /`--'  /
+//     \   `  ;'   :  ||   | '_\.':  | : ;  ; |'--'.     /
+//      :   \ |;   |.' '   : |    '  :  `--'   \ `--'---'
+//       '---" '---'   ;   |,'    :  ,      .-./
+//                     '---'       `--`----'
+//                ,---,    ,---,.           .---.    ,---,.,-.----.
+//        ,---.,`--.' |  ,'  .' |          /. ./|  ,'  .' |\    /  \
+//       /__./||   :  :,---.'   |      .--'.  ' ;,---.'   |;   :    \
+//  ,---.;  ; |:   |  '|   |   .'     /__./ \ : ||   |   .'|   | .\ :
+// /___/ \  | ||   :  |:   :  |-, .--'.  '   \' .:   :  |-,.   : |: |
+// \   ;  \ ' |'   '  ;:   |  ;/|/___/ \ |    ' ':   |  ;/||   |  \ :
+//  \   \  \: ||   |  ||   :   .';   \  \;      :|   :   .'|   : .  /
+//   ;   \  ' .'   :  ;|   |  |-, \   ;  `      ||   |  |-,;   | |  \
+//    \   \   '|   |  ''   :  ;/|  .   \    .\  ;'   :  ;/||   | ;\  \
+//     \   `  ;'   :  ||   |    \   \   \   ' \ ||   |    \:   ' | \.'
+//      :   \ |;   |.' |   :   .'    :   '  |--" |   :   .':   : :-'
+//       '---" '---'   |   | ,'       \   \ ;    |   | ,'  |   |.'
+//                     `----'          '---"     `----'    `---'
 
 // christopher pietsch
 // @chrispiecom
 // 2015-2018
-
 
 utils.welcome();
 
@@ -41,133 +39,128 @@ var search;
 var ping;
 
 if (Modernizr.webgl && !utils.isMobile()) {
-	init();
+  init();
 }
 
 function init() {
+  tags = Tags();
+  canvas = Canvas();
+  search = Search();
+  timeline = Timeline();
+  ping = utils.ping();
 
-	tags = Tags();
-	canvas = Canvas();
-	search = Search();
-	timeline = Timeline()
-	ping = utils.ping();
+  var baseUrl = utils.getDataBaseUrl();
+  console.log(baseUrl);
 
-	var baseUrl = utils.getDataBaseUrl()
-	console.log(baseUrl)
+  d3.json(baseUrl.config || "data/config.json", function (config) {
+    config.baseUrl = baseUrl;
+    utils.initConfig(config);
 
-	d3.json(baseUrl.config || "data/config.json", function (config) {
+    Loader(baseUrl.path + config.loader.timeline).finished(function (timeline) {
+      Loader(baseUrl.path + config.loader.items).finished(function (data) {
+        console.log(data);
 
-		config.baseUrl = baseUrl
-		utils.initConfig(config)
+        utils.clean(data, config.delimiter);
 
-		Loader(baseUrl.path + config.loader.timeline).finished(function (timeline) {
-			Loader(baseUrl.path + config.loader.items).finished(function (data) {
-				console.log(data)
+        tags.init(data, config);
+        search.init();
+        canvas.init(data, timeline, config);
 
-				utils.clean(data, config.delimiter);
+        if (config.loader.layouts) {
+          initLayouts(config);
+        }
 
-				tags.init(data, config);
-				search.init();
-				canvas.init(data, timeline, config);
+        LoaderSprites()
+          .progress(function (textures) {
+            Object.keys(textures).forEach(function (id) {
+              data
+                .filter(function (d) {
+                  return d.id === id;
+                })
+                .forEach(function (d) {
+                  d.sprite.texture = textures[id];
+                });
+            });
+            canvas.wakeup();
+          })
+          //.finished() recalculate sizes
+          .load(baseUrl.path + config.loader.textures.medium.url);
+      });
+    });
+  });
 
-				if (config.loader.layouts) {
-					initLayouts(config);
-				}
+  d3.select(window)
+    .on("resize", function () {
+      if (canvas !== undefined && tags !== undefined) {
+        clearTimeout(window.resizedFinished);
+        window.resizedFinished = setTimeout(function () {
+          canvas.resize();
+          tags.resize();
+        }, 250);
+      }
+    })
+    .on("keydown", function (e) {
+      if (d3.event.keyCode != 27) return;
+      search.reset();
+      tags.reset();
+      canvas.split();
+    });
 
-				LoaderSprites()
-					.progress(function (textures) {
-						Object.keys(textures).forEach(function (id) {
-							data
-								.filter(function (d) {
-									return d.id === id
-								})
-								.forEach(function (d) {
-									d.sprite.texture = textures[id]
-								})
-						})
-						canvas.wakeup()
-					})
-					//.finished() recalculate sizes
-					.load(baseUrl.path + config.loader.textures.medium.url)
-			});
-		});
-	});
+  d3.select(".slidebutton").on("click", function () {
+    var s = !d3.select(".sidebar").classed("sneak");
+    d3.select(".sidebar").classed("sneak", s);
+  });
 
-	d3.select(window)
-		.on("resize", function () {
-			if (canvas !== undefined && tags !== undefined) {
-				clearTimeout(window.resizedFinished);
-				window.resizedFinished = setTimeout(function () {
-					canvas.resize();
-					tags.resize();
-				}, 250);
-			}
-		})
-		.on("keydown", function (e) {
-			if (d3.event.keyCode != 27) return
-			search.reset();
-			tags.reset();
-			canvas.split();
-		})
+  d3.select(".infobutton").on("click", function () {
+    var s = !d3.select(".infobar").classed("sneak");
+    d3.select(".infobar").classed("sneak", s);
+  });
 
-	d3.select(".slidebutton")
-		.on("click", function () {
-			var s = !d3.select(".sidebar").classed("sneak");
-			d3.select(".sidebar").classed("sneak", s);
-		})
+  d3.selectAll(".navi .button").on("click", function () {
+    var that = this;
+    var mode = d3.select(this).attr("data");
+    canvas.setMode(mode);
+    timeline.setDisabled(mode != "time");
 
-	d3.select(".infobutton")
-		.on("click", function () {
-			var s = !d3.select(".infobar").classed("sneak");
-			d3.select(".infobar").classed("sneak", s)
-		})
+    d3.selectAll(".navi .button").classed("active", function () {
+      return that === this;
+    });
+  });
 
-	d3.selectAll(".navi .button")
-		.on("click", function () {
-			var that = this;
-			var mode = d3.select(this).attr("data");
-			canvas.setMode(mode);
-			timeline.setDisabled(mode != "time");
+  function initLayouts(config) {
+    d3.select(".navi").classed("hide", false);
 
-			d3.selectAll(".navi .button").classed("active", function () {
-				return that === this
-			});
-		})
+    console.log(config.loader.layouts);
 
-	function initLayouts(config) {
-		d3.select(".navi").classed("hide", false);
+    config.loader.layouts.forEach((d, i) => {
+      d.title = d.title.toLowerCase();
+      if (d.title === "time") return;
+      d3.csv(baseUrl.path + d.url, function (tsne) {
+        canvas.addTsneData(d.title, tsne);
+      });
+    });
 
-		config.loader.layouts.forEach((d) => {
-			if (d.title === "time") return;
-			d3.csv(baseUrl.path + d.url, function (tsne) {
-				console.log(tsne)
-				canvas.addTsneData(d.title, tsne);
-			});
-		});
+    var s = d3.select(".navi").selectAll(".button").data(config.loader.layouts);
+    s.enter()
+      .append("div")
+      .classed("button", true)
+      .classed("space", (d) => d.space)
+      .text((d) => d.title);
 
-		var layouts = config.loader.layouts;
-		// layouts.unshift({ title: "time" });
-
-		var s = d3.select(".navi").selectAll(".button").data(config.loader.layouts);
-		s.enter()
-			.append("div")
-			.classed("button", true)
-			.classed("space", (d) => d.space)
-			.text((d) => d.title);
-		s.on("click", function (d) {
-			console.log(d.title)
-			canvas.setMode(d.title);
-			timeline.setDisabled(d.title != "time");
-			d3.selectAll(".navi .button").classed(
-				"active",
-				(d) => d.title == canvas.getMode()
-			);
-		});
-		d3.selectAll(".navi .button").classed(
-			"active",
-			(d) => d.title == canvas.getMode()
-		);
-	}
+    s.on("click", function (d) {
+      console.log(d.title);
+      canvas.setMode(d.title);
+      timeline.setDisabled(d.title != "time");
+      d3.selectAll(".navi .button").classed(
+        "active",
+        (d) => d.title == canvas.getMode()
+      );
+    });
+    d3.selectAll(".navi .button").classed(
+      "active",
+      (d) => d.title == canvas.getMode()
+    );
+  }
 }
 
 d3.select(".browserInfo").classed("show", utils.isMobile());
